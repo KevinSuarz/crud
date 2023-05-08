4be8c5d4df98e3274bdd3c17d07ea1d08fce124a
kevin <kevinsuaga@gmail.com>
2023-05-08 09:59:10 +0200

    edit cada registro

--- components/tasks.php
+++ components/tasks.php
@@ -14,15 +14,42 @@
   </tr>
   <?php
     $result = mysqli_query($conn,"SELECT * FROM tasks");
+    if(isset($_GET)){
+      $edit = $_GET['edit'];
+    };
     while($row = mysqli_fetch_assoc($result)){
-      echo
-        "<tr class='tasks__tableRow'>
-          <td>".$row["id"]."</td>
-          <td>".$row["title"]."</td>
-          <td>".$row["description"]."</td>
-          <td>".$row["creationDate"]."</td>
-          <td id='delete' class='delete'><a href='../app/taskDelete.php?id=".$row['id']."'><i class='fa-solid fa-square-xmark'></i></a></td>
-        </tr>";
+        // var_dump($edit);
+        if(isset($edit)){
+          echo
+          "<tr class='tasks__tableRow'>
+            <td>".$row["id"]."</td>
+            <td><input type='text' placeholder=".$row["title"]."></td>
+            <td><input type='text' placeholder=".$row["description"]."></td>
+            <td>".$row["creationDate"]."</td>
+            <td id='delete' class='done'>
+              <a href='../pages/bienvenida.php'>
+                <i class='fa-solid fa-square-check'></i>
+              </a>
+            </td>
+          </tr>";
+          unset($edit);
+        }else {
+          echo
+          "<tr class='tasks__tableRow'>
+            <td>".$row["id"]."</td>
+            <td>".$row["title"]."</td>
+            <td>".$row["description"]."</td>
+            <td>".$row["creationDate"]."</td>
+            <td id='delete' class='delete'>
+              <a href='../app/taskDelete.php?id=".$row['id']."'>
+                <i class='fa-solid fa-square-xmark remove'></i>
+              </a>
+              <a href='../pages/bienvenida.php?edit=".$row['id']."'>
+                <i class='fa-solid fa-edit edit'></i>
+              </a>
+            </td>
+          </tr>";
+        };
     };
   ?>
 </table>
